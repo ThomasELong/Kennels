1eb1d5ee351617015b8c0ce8e69e679f981a4428
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-04-20 14:40:38 -0500

    initial commit

---
+++ src/customer/Customer.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="customer">
+        <h3 className="customer__nameFirst">Steve</h3>
+        <div className="customer__nameLast">Brownlee</div>
+    </section>
+)
---
+++ src/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="location">
+        <h3 className="location__name">Franklin</h3>
+        <div className="location__address">310 Main St</div>
+    </section>
+)
